d2ba43ded0297523290f842f640d06a74b5300b4
JettPy <smel_00@bk.ru>
2023-11-19 20:24:56 +0300

    Правки первой итерации

--- meme_bot.py
+++ meme_bot.py
@@ -1,19 +1,31 @@
 import os
 import dotenv
 import telebot
-
+from telebot.types import Message
 
 dotenv.load_dotenv()
 API_TOKEN = os.getenv("API_TOKEN")
 bot = telebot.TeleBot(API_TOKEN)
 
+
+# Обработчик команды /actions - вывод кнопок для удобного использования бота
+# Автор: Маркина Вера Артёмовна
 @bot.message_handler(commands=["actions"])
-def start(message):
-    reply_menu = telebot.types.ReplyKeyboardMarkup(row_width=1)
-    clue1_button = telebot.types.KeyboardButton("ИЗБРАНАНЫЕ")
-    clue2_button = telebot.types.KeyboardButton("ИСКАТЬ")
-    reply_menu.add(clue1_button, clue2_button)
-    bot.send_message(message.chat.id, "Выберите действие", reply_markup=reply_menu)
+def start(message: Message):
+    inline_menu = telebot.types.InlineKeyboardMarkup(row_width=1)
+    favorites_button = telebot.types.InlineKeyboardButton("ИЗБРАНАНЫЕ", callback_data="favorites")
+    search_button = telebot.types.InlineKeyboardButton("ИСКАТЬ", callback_data="search")
+    # TODO Возможно тут добавим еще одну кнопку
+    inline_menu.add(favorites_button, search_button)
+    bot.send_message(message.chat.id, "Выберите действие:", reply_markup=inline_menu)
+
+
+# Обработчик нажатых кнопок меню
+# Автор: Маркина Вера Артёмовна
+@bot.callback_query_handler(func=lambda call: call.data)
+def button_handlers(call):
+    # TODO Доделать обработку нажатия кнопок
+    pass
 
 
 bot.infinity_polling()
