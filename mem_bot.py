0e6091db87fa032c779d4538f949018c0c837e0b
JettPy <smel_00@bk.ru>
2023-11-19 20:34:20 +0300

    Правки первой итерации

--- mem_bot.py
+++ mem_bot.py
@@ -1,38 +1,46 @@
 import sqlite3
 import telebot
+from telebot.types import Message
 import os
 import dotenv
 
-
 dotenv.load_dotenv()
-API_TOKEN = os.getenv('API_TOKEN')
+API_TOKEN = os.getenv("API_TOKEN")
 bot = (telebot.TeleBot(API_TOKEN))
 
-
-# Подключение базе данных
-conn = sqlite3.connect('memebot.db', check_same_thread=False)
+# Подключение к базе данных
+# Автор: Маркина Ирина Артёмовна
+conn = sqlite3.connect("memebot.db", check_same_thread=False) # Подключение базе данных
 cursor = conn.cursor()
 
-# Создание таблицы для хранения записей
-
+# Создание таблицы для хранения записей пользователей
 cursor.execute('''CREATE TABLE IF NOT EXISTS users
                   (id INTEGER PRIMARY KEY AUTOINCREMENT,
                    username TEXT,
                    password TEXT)''')
 conn.commit() # Подключение к базе данных
 
+# Создание таблицы для хранения записей мемов
 cursor.execute('''CREATE TABLE IF NOT EXISTS memes
                   (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   meme TEXT)''')
-conn.commit() # Подключение к базе данных
+                   meme BLOB)''')
+conn.commit()
 
+# Создание таблицы для хранения записей любимых мемов
 cursor.execute('''CREATE TABLE IF NOT EXISTS favorites
                   (id INTEGER PRIMARY KEY AUTOINCREMENT,
                    user_id INTEGER,
                    meme_id INTEGER)''')
-conn.commit() # Подключение к базе данных
+conn.commit()
 
 
+# Регистрация пользователя в базе данных
+# Автор: Маркина Ирина Артёмовна
+@bot.message_handler(commands=['start'])
+def text(message: Message):
+    # TODO Реализовать работу по регистрации пользователя в базе данных
+    pass
+    # bot.register_next_step_handler()
 
 
 bot.infinity_polling()
