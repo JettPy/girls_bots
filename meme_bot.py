542cd8f3c476b2e62d28ed6749cda0669ffc9239
JettPy <smel_00@bk.ru>
2023-11-26 20:17:31 +0300

    Правки второй итерации

--- meme_bot.py
+++ meme_bot.py
@@ -28,14 +28,54 @@ def button_handlers(call):
     # TODO Доделать обработку нажатия кнопок
     pass
 
+
+# Временная константа:
+images_ids = set()
+
+
+# Команда для генерации мема:
+# 1 этап - выбор изображения
 @bot.message_handler(commands=["meme"])
-def meme_generator(message: Message):
-    response = requests.get("http://api.memegen.link/images/aag/foo/bar.png")
+def get_meme_image(message: Message):
+    global images_ids
+    img_response = requests.get("http://api.memegen.link/templates")
+    images_data = img_response.json()
+    images_ids = {data["id"] for data in images_data}
+    bot.send_message(message.from_user.id, ", ".join(images_ids))
+    bot.send_message(message.from_user.id, "Выберите картинку для мема")
+    bot.register_next_step_handler(message, get_meme_top_text)
+
+
+# 2 этап - выбор текста сверху
+def get_meme_top_text(message: Message):
+    global images_ids
+    image_id = message.text
+    if image_id not in images_ids:
+        bot.send_message(message.from_user.id, "Такой картинки нет, конец операции")
+        return
+    bot.send_message(message.from_user.id, "Введите текст над картинкой")
+    bot.register_next_step_handler(message, get_meme_bottom_text, image_id)
+
+
+# 3 этап - выбор текста снизу
+def get_meme_bottom_text(message: Message, id: str):
+    top_text = message.text
+    bot.send_message(message.from_user.id, "Введите текст под картинкой")
+    bot.register_next_step_handler(message, meme_generator, id, top_text)
+
+
+# 4 этап - генерация мема
+def meme_generator(message: Message, id: str, top_text: str):
+    bottom_text = message.text
+    bot.send_message(message.from_user.id, "Идет генерация, секундочку...")
+    url = f"http://api.memegen.link/images/{id}/{top_text}/{bottom_text}.png"
+    response = requests.get(url)
     image_content = response.content
-    with open("meme.pgp","wb") as file:
+    with open("meme.png", "wb") as file:
         file.write(image_content)
-    with open("meme.pgp", "rb") as file:
-        bot.send_photo(message.from_user.id,file)
+    with open("meme.png", "rb") as file:
+        bot.send_photo(message.from_user.id, file)
+    os.remove("./meme.png")
 
 
 bot.infinity_polling()
